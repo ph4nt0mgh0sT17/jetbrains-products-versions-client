aecdbab00235c0550ea755fa2a053f74d9479970
David Miko <ph4nt0mgh0sT17@gmail.com>
2022-04-18 21:16:58 +0200

    Added distributions and categories.

---
+++ JetBrainsProductsVersionsClient/JetBrainsProductsVersionsClient/Models/Distribution.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JetBrainsProductsVersionsClient.Models;
+
+public class Distribution
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("extension")]
+    public string? Extension { get; set; }
+}
